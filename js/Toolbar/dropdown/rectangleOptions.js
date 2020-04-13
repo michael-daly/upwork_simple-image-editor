30d2f639271e30d430da57af97344a98ec36f058
Michael <mjdaly10@gmail.com>
2020-04-13 03:31:41 -0700

    Froze rectangleOptions

--- js/Toolbar/dropdown/rectangleOptions.js
+++ js/Toolbar/dropdown/rectangleOptions.js
@@ -1,11 +1,13 @@
+import { deepfreeze } from '~/util/deepfreeze.js';
+
 import { RECT_FILL, RECT_OUTLINE } from '~/Toolbar/constants.js';
 
 
-const rectangleOptions =
+const rectangleOptions = deepfreeze (
 [
 	{ label: 'Fill',    value: RECT_FILL    },
 	{ label: 'Outline', value: RECT_OUTLINE },
-];
+]);
 
 
 export { rectangleOptions };
