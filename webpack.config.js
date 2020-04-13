ad0d4fae6ebfad8134aa79f893bab9b5518979a8
Michael <mjdaly10@gmail.com>
2020-04-13 13:50:30 -0700

    Fixed paths

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports =
 {
-	entry: './js/main.js',
+	entry: './client/js/main.js',
 
 	output:
 	{
@@ -41,18 +41,18 @@ module.exports =
 	{
 		alias:
 		{
-			'~/App':             path.resolve (__dirname, './js/App/'),
-			'~/Toolbar':         path.resolve (__dirname, './js/Toolbar/'),
-			'~/ToolbarDropdown': path.resolve (__dirname, './js/Toolbar/dropdown/'),
-			'~/Popup':           path.resolve (__dirname, './js/Popup/'),
-			'~/controls':        path.resolve (__dirname, './js/controls/'),
-			'~/canvas':          path.resolve (__dirname, './js/canvas/'),
-			'~/shapes':          path.resolve (__dirname, './js/canvas/shapes'),
-			'~/MainCanvas':      path.resolve (__dirname, './js/canvas/MainCanvas/'),
-			'~/TempCanvas':      path.resolve (__dirname, './js/canvas/TempCanvas/'),
-			'~/util':            path.resolve (__dirname, './js/util/'),
-			'~/misc':            path.resolve (__dirname, './js/misc/'),
-			'~':                 path.resolve (__dirname, './js/'),
+			'~/App':             path.resolve (__dirname, './client/js/App/'),
+			'~/Toolbar':         path.resolve (__dirname, './client/js/Toolbar/'),
+			'~/ToolbarDropdown': path.resolve (__dirname, './client/js/Toolbar/dropdown/'),
+			'~/Popup':           path.resolve (__dirname, './client/js/Popup/'),
+			'~/controls':        path.resolve (__dirname, './client/js/controls/'),
+			'~/canvas':          path.resolve (__dirname, './client/js/canvas/'),
+			'~/shapes':          path.resolve (__dirname, './client/js/canvas/shapes'),
+			'~/MainCanvas':      path.resolve (__dirname, './client/js/canvas/MainCanvas/'),
+			'~/TempCanvas':      path.resolve (__dirname, './client/js/canvas/TempCanvas/'),
+			'~/util':            path.resolve (__dirname, './client/js/util/'),
+			'~/misc':            path.resolve (__dirname, './client/js/misc/'),
+			'~':                 path.resolve (__dirname, './client/js/'),
 		}
 	},
 };
